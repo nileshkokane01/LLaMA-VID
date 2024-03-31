19520191057776c99ead2f246313475a6815f7ed
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-03-31 10:01:40 +0530

    added forwrd print

--- llamavid/model/builder.py
+++ llamavid/model/builder.py
@@ -21,13 +21,20 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, BitsAn
 import torch
 from llamavid.model import *
 from llamavid.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
+import inspect
 
+def print_file_and_line_info():
+    frame_info = inspect.stack()[1]
+    file_name = frame_info.filename
+    line_number = frame_info.lineno
+    print("File:", file_name, "Line:", line_number)
 
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", device="cuda"):
     kwargs = {"device_map": device_map}
 
     if load_8bit:
         kwargs['load_in_8bit'] = True
+        print_file_and_line_info()
     elif load_4bit:
         kwargs['load_in_4bit'] = True
         kwargs['quantization_config'] = BitsAndBytesConfig(
@@ -36,14 +43,17 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type='nf4'
         )
+        print_file_and_line_info()
+        
     else:
         kwargs['torch_dtype'] = torch.float16
+        
 
     if 'vid' in model_name.lower():
         # Load LLaMA-VID model
         if model_base is not None:
             # this may be mm projector only
-            print('Loading LLaVA from base model...')
+            print_file_and_line_info()
             tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             cfg_pretrained = AutoConfig.from_pretrained(model_path)
             model = LlavaLlamaAttForCausalLM.from_pretrained(model_base, low_cpu_mem_usage=True, config=cfg_pretrained, **kwargs)
@@ -51,14 +61,17 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             mm_projector_weights = torch.load(os.path.join(model_path, 'mm_projector.bin'), map_location='cpu')
             mm_projector_weights = {k: v.to(torch.float16) for k, v in mm_projector_weights.items()}
             model.load_state_dict(mm_projector_weights, strict=False)
+           
         else:
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
             model = LlavaLlamaAttForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
+            print_file_and_line_info()
 
     else:
         # Load language model
         if model_base is not None:
             # PEFT model
+            print_file_and_line_info()
             from peft import PeftModel
             tokenizer = AutoTokenizer.from_pretrained(model_base, use_fast=False)
             model = AutoModelForCausalLM.from_pretrained(model_base, torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map="auto")
@@ -71,35 +84,44 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         else:
             use_fast = False
             if 'mpt' in model_name.lower():
+                print_file_and_line_info()
                 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
                 model = AutoModelForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, trust_remote_code=True, **kwargs)
             else:
+                print_file_and_line_info()
                 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
                 model = AutoModelForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
 
     image_processor = None
 
     if 'vid' in model_name.lower():
+        print_file_and_line_info()
         mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
         mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
         if mm_use_im_patch_token:
+            print_file_and_line_info()
             tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
         if mm_use_im_start_end:
+            print_file_and_line_info()
             tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
         model.resize_token_embeddings(len(tokenizer))
 
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
+            print_file_and_line_info()
             vision_tower.load_model()
         vision_tower.to(device=device, dtype=torch.float16)
         image_processor = vision_tower.image_processor
         # initialize attention modules
         model.config.model_path = model_path
         model.get_model().initialize_attention_modules(model.config, for_eval=True)
+        print_file_and_line_info()
 
     if hasattr(model.config, "max_sequence_length"):
+        print_file_and_line_info()
         context_len = model.config.max_sequence_length
     else:
+        print_file_and_line_info()
         context_len = 2048
 
     return tokenizer, model, image_processor, context_len
