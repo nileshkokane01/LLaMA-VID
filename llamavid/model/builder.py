4fd39c1481dbd5e07b5674ef358700df084c58a3
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-04-22 12:01:01 +0530

    dump print

--- llamavid/model/builder.py
+++ llamavid/model/builder.py
@@ -156,6 +156,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
             model.load_state_dict(mm_projector_weights, strict=False)
            
         else:
+            print('====model _  name : ' , model_path)
             tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
             model = LlavaLlamaAttForCausalLM.from_pretrained(model_path, low_cpu_mem_usage=True, **kwargs)
             
